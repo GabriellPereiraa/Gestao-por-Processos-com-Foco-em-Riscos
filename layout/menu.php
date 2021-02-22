0701979f9a5ea2915435496797212174cc59eef9
Gabriel Pereira <78962152+GabriellPereiraa@users.noreply.github.com>
2021-02-22 10:04:20 -0300

    Start

---
+++ layout/menu.php
@@ -0,0 +1,108 @@
+<body>
+    <div id="app">
+        <div id="sidebar">
+            <div class="sidebar-wrapper active ps ps--active-y">
+                <div class="m-4">
+                    <a href="index.php">
+                        <img class="img-fluid" src="http://ava.egp.ce.gov.br/2018/desv/logo/egp.png" alt="Logo EGPCE" srcset="">
+                    </a>
+                </div>
+                <div class="sidebar-menu ">
+                    <ul class="menu">
+                        <!--  <li class="sidebar-item">
+                        <a href="../Apresentacao.php" class="sidebar-link">
+                            <i data-feather="home" width="20"></i>
+                            <span>Apresentação</span>
+                        </a>
+                    </li> -->
+                        <li class="sidebar-item">
+                            <a href="Apresentacao.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Apresentação</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico01.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>1.0 - Ética e serviço público</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico02.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>2.0 - Alguns princípios</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Topico03.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>3.0 - Antecedentes da ética no serviço público</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-item has-sub">
+                            <a href="" class='sidebar-link'>
+                                <i data-feather="home" width="20"></i>
+                                <span>4.0 - Modelos de ética</span>
+                            </a>
+                            <ul class="submenu">
+                                <li class="sidebar-item">
+                                    <a href="Topico04.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.1 - Modelos de ética multinacionais: A OCDE</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico04.2.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.2 - Ferramentas estratégicas</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico04.3.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.3 - Ética nas Organizações</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico04.4.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.4 - Governança Corporativa</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico04.5.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.5 - Padrão britânico de conduta na vida pública</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.6 - Outras experiências estrangeiras</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico04.7.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.7 - Modelos éticos na América Latina</span>
+                                    </a>
+                                </li>
+                                <li class="sidebar-item">
+                                    <a href="Topico04.8.php" class="sidebar-sub">
+                                        <i data-feather="home" width="20"></i>
+                                        <span>4.8 - Exemplos de códigos de ética</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="sidebar-item">
+                            <a href="Referencias.php" class="sidebar-link">
+                                <i data-feather="home" width="20"></i>
+                                <span>Referências</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
